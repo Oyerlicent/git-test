565430661b65f0a99965005274c07f3373683ae4
Oyerlicent <2504404271@qq.com>
2022-08-17 22:16:16 +0800

    first commit

---
+++ src/main/java/com/Oyerlicent/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.Oyerlicent.git;
+
+/**
+ * @author YinLiang
+ * @create 2022-08-17-22:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
